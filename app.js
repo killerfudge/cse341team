cd8d0ae15bf60ef41caf056a1de22167be26f1a3
Brandon Peterson <brandonacfuntech@gmail.com>
2022-03-10 22:59:17 -0600

    Re-adjusted to be setup as Rest API.

--- app.js
+++ app.js
@@ -15,11 +15,14 @@ const MONGODB_URL = process.env.MONGODB_URL;
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 // TODO use imported routes
 app.use(budgetRoutes);
